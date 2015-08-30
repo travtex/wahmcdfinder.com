211a4fabe0322b381e10d9ffb778f6d363f7609b
Travis Flatt <tflatt74@gmail.com>
2015-08-30 00:09:22 -0500

    Bootstrap, jQuery added via bower/gulp

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,21 @@ var elixir = require('laravel-elixir');
  |
  */
 
+var bowerDir = './resources/assets/vendor/';
+
+var lessPaths = [
+	bowerDir + "bootstrap/less",
+	bowerDir + "font-awesome/less",
+	bowerDir + "bootstrap-select/less"
+];
+
 elixir(function(mix) {
-    mix.sass('app.scss');
+	mix.less('app.less', 'public/css', { paths: lessPaths })
+		.scripts([
+			'jquery/dist/jquery.min.js',
+			'bootstrap/dist/js/bootstrap.min.js',
+			'bootstrap-select/dist/js/bootstrap-select.min.js'
+			], 'public/jus/vendor.js', bowerDir)
+		.copy('resources/assets/js/app.js', 'public/js/app.js')
+		.copy(bowerDir + 'font-awesome/fonts', 'public/fonts');
 });
